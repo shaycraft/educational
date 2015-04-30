2de81e3e7904e440fb4a1e2c7221a747b856c29c
Sam Haycraft <phreakdawg@gmail.com>
2015-04-30 15:25:58 -0600

    first part of unit 5 changes, doesn't work

---
+++ PluralSIghtGithubChallenge/app.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    var app = angular.module("githubViewer", ["ngRoute"]);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+        .when("/main", {
+            templateUrl: "main.html",
+            controller: "MainController"
+        })
+        .otherwise({ redirectTo: "/main" });
+    });
+})();
