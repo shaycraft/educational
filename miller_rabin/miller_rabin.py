b08fd2f0f0cc19312dd2c0ddf0fa5e5720ea89df
Sam Haycraft <shaycraft@metlife.com>
2016-05-04 16:13:13 -0600

    Improved calculation of 2 ** s * d

--- miller_rabin/miller_rabin.py
+++ miller_rabin/miller_rabin.py
@@ -1,32 +1,37 @@
-import math
 import random
 
 
 def calc_p2(num):
-    if num % 2 == 1:
-        raise "ERROR:  num must be even"
-    for d in range(1, num + 1, 2):
-        print d
-        r = math.log(float(num)/float(d)) / math.log(2)
-        print r
-        if r.is_integer():
-            return {'r': int(r), 'd': int(d)}
+    remainder = -1
+    r = 0
+    d = num
+    while remainder != 1:
+        quotient, remainder = divmod(d, 2)
+        if remainder != 1:
+            r += 1
+            d = quotient
+    assert(2**r * d == num)
+    return {'r': int(r), 'd': int(d)}
 
 
 # n is number to test for primality, k is accuracy
 def miller_rabin(n, k):
+    assert n >= 2
+    if n == 2:
+        return "Definitely prime"
+    elif n % 2 == 0:
+        return "Definitely composite"
+
     vals = calc_p2(n - 1)
     r = vals['r']
     d = vals['d']
     for i in range(0, k):
-        print "DEBUG:  beginning of i while loop"
         a = random.choice([x for x in range(2, n - 1)])
         # the following should be improved with repeated squaring-modular exponentiation for real-world usage
         x = pow(a, d) % n
         if x != 1 and x != n - 1:
             stop = False
             for j in range(0, r - 1):
-                print "DEBUG:  beginning of j while loop"
                 x = pow(x, 2) % n
                 if x == 1:
                     print "Composite"
@@ -40,4 +45,4 @@ def miller_rabin(n, k):
     return
 
 
-miller_rabin(223, 60)
+miller_rabin(7881, 60)
