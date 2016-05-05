4a63004fafdf32b845769c76f77d72775bc57a6c
Sam Haycraft <shaycraft@metlife.com>
2016-05-05 09:34:41 -0600

    Debug statements, fixes stop condiion

--- miller_rabin/miller_rabin.py
+++ miller_rabin/miller_rabin.py
@@ -14,6 +14,19 @@ def calc_p2(num):
     return {'r': int(r), 'd': int(d)}
 
 
+def modular_pow(base, exponent, modulus):
+    if modulus == 1:
+        return 0
+    result = 1
+    base %= modulus
+    while exponent > 0:
+        if exponent % 2 == 1:
+            result = (result * base) % modulus
+        exponent >>= 1
+        base = (base * base) % modulus
+    return result
+
+
 # n is number to test for primality, k is accuracy
 def miller_rabin(n, k):
     assert n >= 2
@@ -25,24 +38,31 @@ def miller_rabin(n, k):
     vals = calc_p2(n - 1)
     r = vals['r']
     d = vals['d']
+    print "DEBUG:  r = {0}, d = {1}".format(r, d)
     for i in range(0, k):
-        a = random.choice([x for x in range(2, n - 1)])
+        #a = random.choice([x for x in range(2, n - 1)])
+        a = random.randrange(2, n-1)
+        print "random a = {0}".format(a)
         # the following should be improved with repeated squaring-modular exponentiation for real-world usage
-        x = pow(a, d) % n
+        #x = pow(a, d) % n
+        x = modular_pow(a, d, n)
+        print "DEBUG:  modular_power(a, d, n) = {0}".format(x)
         if x != 1 and x != n - 1:
             stop = False
             for j in range(0, r - 1):
-                x = pow(x, 2) % n
+                #x = pow(x, 2) % n
+                x = modular_pow(x, 2, n)
+                print "DEBUG:  modular_pow(x, 2, n) = {0}".format(x)
+                #this following is not right, conside num = 41 or num = 67,280,421,310,721
                 if x == 1:
-                    print "Composite"
-                    return
+                    return "x == 1 Composite"
                 if x == n - 1:
                     stop = True
+                    break
             if not stop:
-                print "Composite"
-                return
-    print "probably prime"
-    return
+                return "stop Composite"
+    return "probably prime"
 
 
-miller_rabin(7881, 60)
+print miller_rabin(long('41'), 60)
+#print modular_pow(4, 13, 497)
